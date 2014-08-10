9381a21b155c4cd212576606979958b70925a18a
rezoana <nirobota2011@hotmail.com>
2014-08-10 12:46:47 +0600

    Update getRecentPostInfo.php

--- Model/getRecentPostInfo.php
+++ Model/getRecentPostInfo.php
@@ -1,4 +1,9 @@
  <?php
  
+include ('C:\wamp\www\SEGreenCity\Controller\connectionController.php');
 
+$query="Select *,COUNT(*) FROM article INNER JOIN categories ON categoryID=catid WHERE DATE_SUB(CURDATE(),INTERVAL 30 DAY) GROUP BY date_posted DESC LIMIT 10 ";
+
+$blog = mysql_query($query) or die(mysql_error());
+$num_blog = mysql_num_rows($blog);
 ?>
