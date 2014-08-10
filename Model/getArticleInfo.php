f6d325c7722b4af4eed15d62a964132e3c3865e1
Zarrin Tasnim Asha <asha.ku@hotmail.com>
2014-08-10 12:45:44 +0600

    Update getArticleInfo.php

--- Model/getArticleInfo.php
+++ Model/getArticleInfo.php
@@ -1,4 +1,11 @@
 <?php
+include ('C:\wamp\www\SEGreenCity\Controller\connectionController.php');
 
+$query1="Select *,DATE_FORMAT(date_posted,'%W,%d %b %Y') as thedate FROM article INNER JOIN categories ON categoryID=catid WHERE DATE_SUB(CURDATE(),INTERVAL 30 DAY) ORDER BY date_posted DESC LIMIT 5";
+
+$blogarticles = mysql_query($query1) or die(mysql_error());
+$num = mysql_num_rows($blogarticles);
+$row_articles = '';
+$row_blog = '';
 
 ?>
